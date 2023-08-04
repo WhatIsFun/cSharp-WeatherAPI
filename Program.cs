7eacfe5b597f74517265ffb1a157e635ecca400e
WhatIsFun <mohammed110051@gmail.com>
2023-08-04 17:37:03 +0400

    cSharp-WeatherAPI

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace cSharp_WeatherAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            try
+            {
+                String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+                String parameters = $"{baseUrl}?q={Uri.EscapeDataString("new york")}&appid=3df7da9e2ea55fc1cf78aa3d743591ea";
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage respone = await client.GetAsync(baseUrl);
+                    if(respone.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await respone.Content.ReadAsStringAsync();
+                        Console.WriteLine(jsonResponse);
+                    }
+                }
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
